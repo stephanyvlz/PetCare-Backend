83be296b606220a7a19ffd1590311d2b3519c037
stephanyvlz <5323100091@correcaminos.uts.edu.mx>
2026-04-09 01:06:08 -0700

    Se agregaron los Includes faltantes en AppointmentRepository de los cambios en el comit de Alvaro v2.1.1

--- Repositories/AppointmentRepository.cs
+++ Repositories/AppointmentRepository.cs
@@ -21,6 +21,7 @@ public class AppointmentRepository : IAppointmentRepository
 
     public Task<List<Appointment>> GetByUserAsync(Guid id_user) =>
         _db.Appointment
+           .Include(c => c.User)
            .Include(c => c.Pet)
            .Include(c => c.Clinic)
            .Include(c => c.veterinarian)
@@ -32,6 +33,7 @@ public class AppointmentRepository : IAppointmentRepository
            .Include(c => c.User)
            .Include(c => c.Pet)
            .Include(c => c.Clinic)
+           .Include(c => c.veterinarian)
            .Where(c => c.id_veterinarian == id_veterinarian)
            .ToListAsync();
 
